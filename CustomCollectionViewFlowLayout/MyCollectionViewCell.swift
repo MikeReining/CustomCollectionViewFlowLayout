e008a68dc9f81bc78966b3ed0d259e6f469b9c26
Mike Reining <emailmike@gmail.com>
2015-01-31 17:12:59 -0800

    Added demo resources to project

---
+++ CustomCollectionViewFlowLayout/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  CustomCollectionViewFlowLayout
+//
+//  Created by Michael Reining on 1/31/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
